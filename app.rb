e29c046d14fc663971415050aa604a6f330471cf
Stanis1av <krasavin.stanislaus@gmail.com>
2020-10-04 19:25:48 +0400

    add method post and appeal to params

--- app.rb
+++ app.rb
@@ -3,6 +3,15 @@ require 'sinatra'
 require 'sinatra/reloader'
 
 get '/' do
+  erb 'Welcome user! '
+end
+
+get '/new' do
+  erb :new
+end
+
+post '/new' do
+  @content = params[:content]
   erb :new
 end
 
