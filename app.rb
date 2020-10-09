408eb8816fe262d590868b833b87f3b6f8ab3ef1
Stanis1av <krasavin.stanislaus@gmail.com>
2020-10-09 20:26:23 +0400

    correct to app.rb for using ActiveRecord

--- app.rb
+++ app.rb
@@ -2,38 +2,35 @@ require 'rubygems'
 require 'sinatra'
 require 'sinatra/reloader'
 require 'sqlite3'
+require 'sinatra/activerecord'
 
-def init_db
-  @db = SQLite3::Database.new 'blog.db'
-  @db.results_as_hash = true
+set :database, {adapter: "sqlite3", database: "blog.db"}
+
+class Post < ActiveRecord::Base
 end
 
-before do
-  init_db
-end
+# configure do
+#   init_db
+#   @db.execute 'CREATE TABLE if not exists Posts
+#   (
+#     id INTEGER,
+#     name_autor TEXT,
+#     created_date DATE,
+#     content TEXT,
+#     PRIMARY KEY(id AUTOINCREMENT)
+#   );'
 
-configure do
-  init_db
-  @db.execute 'CREATE TABLE if not exists Posts
-  (
-    id INTEGER,
-    name_autor TEXT,
-    created_date DATE,
-    content TEXT,
-    PRIMARY KEY(id AUTOINCREMENT)
-  );'
-
-  @db.execute 'CREATE TABLE if not exists Comments
-  (
-    id INTEGER PRIMARY KEY AUTOINCREMENT,
-    created_date DATE,
-    content TEXT,
-    post_id integer
-  );'
-end
+#   @db.execute 'CREATE TABLE if not exists Comments
+#   (
+#     id INTEGER PRIMARY KEY AUTOINCREMENT,
+#     created_date DATE,
+#     content TEXT,
+#     post_id integer
+#   );'
+# end
 get '/' do
-  @results = @db.execute 'select * from Posts order by id desc'
-  erb :index
+  # @results = @db.execute 'select * from Posts order by id desc'
+  # erb :index
 end
 
 get '/new' do
@@ -41,31 +38,31 @@ get '/new' do
 end
 
 post '/new' do
-  @content = params[:content]
-  @name_autor = params[:name_autor]
+  # @content = params[:content]
+  # @name_autor = params[:name_autor]
 
-  if @content.length < 1
-    @error = 'Type post text'
-    return erb :new
-  end
+  # if @content.length < 1
+  #   @error = 'Type post text'
+  #   return erb :new
+  # end
 
-  @db.execute 'insert into Posts (name_autor, content, created_date) values (?, ?, datetime())', [@name_autor, @content]
+  # @db.execute 'insert into Posts (name_autor, content, created_date) values (?, ?, datetime())', [@name_autor, @content]
 
-  # Перенаправление на главную страницу
-  redirect to '/'
+  # # Перенаправление на главную страницу
+  # redirect to '/'
 end
 
 get '/details/:post_id' do
-  # Получаю переменную из URL,ф
-  post_id = params[:post_id]
-  # Получаю список постов
-  # у нас  только 1 пост
-  results = @db.execute 'select * from Posts where id = ?', [post_id]
-  # Выбираю этот 1 пост в переменную @row
-  @row = results[0]
-  # Выбираем комментарии для моего поста
-  @comments = @db.execute 'select * from Comments where post_id = ? order by id', [post_id]
-  # Возвращаю пердставление details.erb
+  # # Получаю переменную из URL,ф
+  # post_id = params[:post_id]
+  # # Получаю список постов
+  # # у нас  только 1 пост
+  # results = @db.execute 'select * from Posts where id = ?', [post_id]
+  # # Выбираю этот 1 пост в переменную @row
+  # @row = results[0]
+  # # Выбираем комментарии для моего поста
+  # @comments = @db.execute 'select * from Comments where post_id = ? order by id', [post_id]
+  # # Возвращаю пердставление details.erb
   erb :details
 end
 
@@ -73,28 +70,28 @@ end
 # (браузер отправляет данные на сервер, мы их принимаем)
 post '/details/:post_id' do
 
-    post_id = params[:post_id]
-    content = params[:content]
+  #   post_id = params[:post_id]
+  #   content = params[:content]
 
-    # if content.length < 1
-    #   @error = 'Type text comment'
-    #   return erb :index
-    # end
-      @db.execute 'insert into Comments
-      (
-        content,
-        created_date,
-        post_id
-      )
-        values
-      (
-        ?,
-        datetime(),
-        ?
-      )', [content, post_id]
+  #   # if content.length < 1
+  #   #   @error = 'Type text comment'
+  #   #   return erb :index
+  #   # end
+  #     @db.execute 'insert into Comments
+  #     (
+  #       content,
+  #       created_date,
+  #       post_id
+  #     )
+  #       values
+  #     (
+  #       ?,
+  #       datetime(),
+  #       ?
+  #     )', [content, post_id]
 
 
-  # Перенаправление на главную страницу
-  redirect to ('/details/' + post_id)
+  # # Перенаправление на главную страницу
+  # redirect to ('/details/' + post_id)
 end
 
